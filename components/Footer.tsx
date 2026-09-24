d9feba55aabe146d82c4883467496c656a6e368d
ahoora <ahoora.ala@gmail.com>
2026-09-24 19:16:46 +0330

    Fix footer contact links after Preferred Sources update

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -66,7 +66,7 @@ export default function Footer() {
                 <Sparkles size={13} /> افزودن به منابع ترجیحی گوگل
               </a>
               <a
-                href="https://wa.me/989122606778?text=%D8%B3%D9%84%D8%A7%D9%85%D8%8C%20%D8%A7%D8%B2%20%D8%B3%D8%A7%DB%8C%D8%AA%20%D8%AE%D9%88%D9%86%D9%87%E2%80%8C%D9%85%D8%A7%20%D9%BE%DB%8C%D8%A7%D9%85%20%D9%85%DB%8C%E2%80%8C%D8%AF%D9%87%D9%85."
+                href="https://wa.me/989122606778?text=%D8%B3%D9%84%D8%A7%D9%85%D8%8C%20%D8%A7%D8%B2%20%D8%B3%D8%A7%DB%8C%D8%AA%20%D8%AE%D9%88%D9%86%D9%87%E2%80%8C%D9%86%D9%85%D8%A7%20%D9%BE%DB%8C%D8%A7%D9%85%20%D9%85%DB%8C%E2%80%8C%D8%AF%D9%87%D9%85."
                 target="_blank"
                 rel="noopener noreferrer"
               >
@@ -91,7 +91,7 @@ export default function Footer() {
               Ahoora Studio <ExternalLink size={12} />
             </a>
             <a
-              href="https://wa.me/989122606778?text=%D8%B3%D9%84%D8%A7%D9%85%D8%8C%20%D8%A7%D8%B2%20%D8%B3%D8%A7%DB%8C%D8%AA%20%D8%AE%D9%88%D9%86%D9%87%E2%80%8C%D9%86%D8%A7%20%D8%A8%D8%B1%D8%A7%DB%8C%20%D8%AA%D9%85%D8%A7%D8%B3%20%D8%A8%D8%A7%20Ahoora%20Studio%20%D9%BE%DB%8C%D8%A7%D9%85%20%D9%85%DB%8C%E2%80%8C%D8%AF%D9%87%D9%85."
+              href="https://wa.me/989122606778?text=%D8%B3%D9%84%D8%A7%D9%85%D8%8C%20%D8%A7%D8%B2%20%D8%B3%D8%A7%DB%8C%D8%AA%20%D8%AE%D9%88%D9%86%D9%87%E2%80%8C%D9%86%D9%85%D8%A7%20%D8%A8%D8%B1%D8%A7%DB%8C%20%D8%AA%D9%85%D8%A7%D8%B3%20%D8%A8%D8%A7%20Ahoora%20Studio%20%D9%BE%DB%8C%D8%A7%D9%85%20%D9%85%DB%8C%E2%80%8C%D8%AF%D9%87%D9%85."
               target="_blank"
               rel="noopener noreferrer"
             >
